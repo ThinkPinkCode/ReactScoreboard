73f5001efd3205530dea1583493c1b676331b853
Melanie Myers <melanie.myers@365smartshop.com>
2016-11-11 10:16:52 -0500

    added player components

--- app.jsx
+++ app.jsx
@@ -47,53 +47,10 @@ function Application(props) {
     <Header title ={props.title} />
     
         <div className = "players">
-          <div className = "player">
-              <div className = "player-name">
-                Melanie Myers
-              </div>
-    
-              <div className = "player-score">
-    
-                  <div className = "counter">
-                      <button className = "counter-action decrement"> - </button>
-                      <div className = "counter-score"> 31 </div>
-                      <button className = "counter-action increment"> + </button>
-                   </div>
-              </div>
-          </div>
-    
-    
-    <div className = "player">
-              <div className = "player-name">
-                Allison Myers
-              </div>
-    
-              <div className = "player-score">
-    
-                  <div className = "counter">
-                      <button className = "counter-action decrement"> - </button>
-                      <div className = "counter-score"> 56 </div>
-                      <button className = "counter-action increment"> + </button>
-                   </div>
-              </div>
-          </div>
-    
-    
-    
-    <div className = "player">
-              <div className = "player-name">
-                Elizabeth Yunker
-              </div>
-    
-              <div className = "player-score">
-    
-                  <div className = "counter">
-                      <button className = "counter-action decrement"> - </button>
-                      <div className = "counter-score"> 77 </div>
-                      <button className = "counter-action increment"> + </button>
-                   </div>
-              </div>
-          </div>
+            <Player name = "Melanie Myers" score={31} />
+            <Player name="Allison Myers" score={56}/>
+            <Player name="Elizabeth Yunker" score={77}/>
+
         </div>
     
     </div>
