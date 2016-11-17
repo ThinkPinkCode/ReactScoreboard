f4db9fa24120e90c79b281255a51ca13dcdc9426
Melanie Myers <melanie.myers@365smartshop.com>
2016-11-17 10:13:00 -0500

    Stopwatch Reset button functional

--- app.jsx
+++ app.jsx
@@ -61,6 +61,10 @@ var Stopwatch = React.createClass({
     },
 
     onReset: function () {
+        this.setState({
+            elapsedTime: 0,
+            previousTime: Date.now(),
+        });
 
     },
 
